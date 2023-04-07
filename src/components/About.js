2413f5b8522bee1d294254e94e246d773b9f8842
chystarr <chynnastarr2@gmail.com>
2023-04-07 13:17:56 -0400

    Set up layout for About page using Material UI components

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,38 @@
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
 function About() {
     return (
-      <div>
+      <div> {/* Replace outer div element with ThemeProvider component later */}
         <p>Information about the doctor (how long have they been practicing, any certifications
         or specialties, where they went to school) and the practice itself. You can include
         photos of the “office” in this component.</p>
+
+        <Container>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: {xs: 'column', md: 'row'},
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              '& > :not(style)': {
+                width: 400,
+                height: 400,
+                my: 6,
+              },
+            }}
+          >
+            <Paper variant='outlined'>
+              text here
+            </Paper>
+            <Paper variant='outlined'>
+              photo here
+            </Paper>
+          </Box>
+        </Container>
       </div>
+      
     ) ;  
   }
   
